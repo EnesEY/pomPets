99bce9340d4ace294b7db510ce4497d41a8e8020
Enes.Yaldiz <enes.yaldiz.bevelop@hd.digital>
2020-12-15 02:37:38 +0100

    initial commit for production

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 module.exports = {
   // configure the code below with your username, password and mlab database information
-  database: 'mongodb://<username>:<password>@ds12226.mlab.com:12226/meanauthapp',   //prod
-  //database: 'mongodb://localhost:27017/meanauth',    //dev
+  database: 'mongodb+srv://EnesEY:a1625a651e0a77e4d428de839a7d0e1c7971a481688ff676dbd254c07dbb1ca7@cluster0.ep6wv.mongodb.net/test',   //prod
+  //database: 'mongodb://localhost:27017/pomPets',    //dev
   secret: 'yoursecret'
 }
