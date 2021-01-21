962bca07558355465a77cddc8deab827091699f7
enesey <enes.yaldiz94@gmail.com>
2021-01-22 00:16:01 +0100

    added-timer-functionality

---
+++ angular-src/src/app/components/action-buttons/action-buttons.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { TimerService } from '../../services/timer.service';
+
+@Component({
+  selector: 'app-action-buttons',
+  templateUrl: './action-buttons.component.html',
+  styleUrls: ['./action-buttons.component.css']
+})
+export class ActionButtonsComponent {
+
+  constructor(
+    private titleService: Title,
+    private timerService: TimerService
+  ) {}
+
+  /**
+   * Toggle play/pause of timer
+   */
+  toggleTimer(){
+    if (this.timerService.isRunning().getValue()){
+      this.timerService.pauseTimer();
+    } else {
+      this.timerService.startTimer();
+    }
+  }
+
+  /**
+   * Restarts timer
+   */
+  restartTimer(){
+    this.timerService.restartTimer();
+  }
+
+}
---
+++ angular-src/src/app/views/timer-view/timer-view.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { TimerService } from '../../services/timer.service';
+import { SettingsService } from '../../services/settings.service';
+
+@Component({
+  selector: 'app-timer-view',
+  templateUrl: './timer-view.component.html'
+})
+export class TimerViewComponent implements OnInit {
+
+  constructor(
+    private titleService: Title,
+    private timerService: TimerService,
+    private settingsService: SettingsService
+  ) { }
+
+  ngOnInit() {
+    this.timerService.setTimer(this.settingsService.timerTypes[0]);
+  }
+
+}
